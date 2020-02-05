481767d97f34f3afb65bc903139f368572f7825b
TempSchoolAcct <54554396+TempSchoolAcct@users.noreply.github.com>
2020-02-05 13:43:44 -0600

    Add files via upload
    These files contain the actual code and resources for the project that weren't uploaded with the first commit for whatever reason.

---
+++ Pods/Target Support Files/Pods-Flixters-FlixtersUITests/Pods-Flixters-FlixtersUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Flixters_FlixtersUITests : NSObject
+@end
+@implementation PodsDummy_Pods_Flixters_FlixtersUITests
+@end
---
+++ Pods/Target Support Files/Pods-FlixtersTests/Pods-FlixtersTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_FlixtersTests : NSObject
+@end
+@implementation PodsDummy_Pods_FlixtersTests
+@end
